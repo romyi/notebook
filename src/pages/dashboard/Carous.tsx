680d57fdc0652abcb86d3274f93bba467850f72b
Roman Klimov <raklimov@sberbank.ru>
2022-10-12 23:22:51 +0300

    carousel with user-defined card widths

--- src/pages/dashboard/Carous.tsx
+++ src/pages/dashboard/Carous.tsx
@@ -1,25 +1,24 @@
 import React, { ReactNode, useMemo, useReducer, useRef } from 'react'
 import { useSwipeable } from 'react-swipeable';
 import { CarouselBlock, CarouselHolder } from './Carous.styled'
-
 interface ICarous {
   children: ReactNode[],
-  cards?: number
+  cards?: number[]
 }
 
-const buildCarouselReducer = (total: number, cards: number) => (state: any, action: any) => {
+const buildCarouselReducer = (total: number, cards: number[]) => (state: any, action: any) => {
   switch (action.type) {
     case 'left':
-      if (state.ix !== total) {
+      if (state.ix !== total - 1) {
         return {
-          delta: state.delta - 100/cards,
+          delta: state.delta - 100/cards[state.ix],
           ix: state.ix + 1,
         }
       } else return state
     case 'right':
-      if (state.ix !== 1) {
+      if (state.ix !== 0) {
         return {
-          delta: state.delta + 100/cards,
+          delta: state.delta + 100/cards[state.ix - 1],
           ix: state.ix - 1,
         }
       } else return state
@@ -29,9 +28,9 @@ const buildCarouselReducer = (total: number, cards: number) => (state: any, acti
 }
 
 const Carous = ({children, cards}: ICarous) => {
-  const wrchildren = React.Children.map(children, child => <CarouselBlock cards={cards}>{child}</CarouselBlock>);
-  const carouselReducer = useMemo(() => buildCarouselReducer(React.Children.count(children), cards ?? 1), []);
-  const [state, dispatch] = useReducer(carouselReducer, {delta: 0, ix: 1}) 
+  const wrchildren = React.Children.map(children, (child, ix) => <CarouselBlock cards={cards ? cards[ix] : 1.3}>{child}</CarouselBlock>);
+  const carouselReducer = useMemo(() => buildCarouselReducer(React.Children.count(children), cards ?? Array(children.length).fill(1.3)), []);
+  const [state, dispatch] = useReducer(carouselReducer, {delta: 0, ix: 0})
   const handlers = useSwipeable({
     onSwipedLeft: () => {
       dispatch({type: 'left' })
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -5,7 +5,7 @@ import { CarouselPod } from './Carous.styled'
 const Dashboard = () => {
   return (
     <Mobile>
-      <Carous cards={1.4}>
+      <Carous cards={[1.6, 1, 1.4]}>
         <CarouselPod><h2>1</h2></CarouselPod>
         <CarouselPod><h2>2</h2></CarouselPod>
         <CarouselPod><h1>4</h1></CarouselPod>
